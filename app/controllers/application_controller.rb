255f622eb8b1c0e528dd9b3c3fb4d07c4bec67c5
Leo Wilkin <leo@wilkin.xyz>
2026-01-17 23:19:30 +0000

    properly render 404 on hidden events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,15 @@ class ApplicationController < ActionController::Base
 
   allow_browser versions: :modern
 
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
   def pundit_user
     current_admin
+  end
+
+  private
+
+  def render_not_found
+    render file: Rails.public_path.join("404.html"), status: :not_found, layout: false
   end
 end
